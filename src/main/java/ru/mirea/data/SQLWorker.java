850e46d84c259d7704b2be7b4a9d1a61d5a2672f
Aglophotis <Atisphatis@gmail.com>
2018-09-19 17:34:59 +0300

    Small features

--- src/main/java/ru/mirea/data/SQLWorker.java
+++ src/main/java/ru/mirea/data/SQLWorker.java
@@ -3,32 +3,57 @@ package ru.mirea.data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.sql.Statement;
 
 public class SQLWorker {
-    private String userName = "root";
-    private String password = "xxxx";
-    private String dbms = "mysql";
-    private String serverName = "192.168.1.1";
-    private String portNumber = "3306";
+    private final String driverName = "org.sqlite.JDBC";
+    private final String connectionString = "jdbc:sqlite:C:\\DB\\stuffs.db";
 
-    public Connection getConnection() throws SQLException {
-        Connection conn = null;
-        Properties connectionProps = new Properties();
-        connectionProps.put("user", this.userName);
-        connectionProps.put("password", this.password);
-
-        if (this.dbms.equals("mysql")) {
-            conn = DriverManager.getConnection(
-                    "jdbc:" + this.dbms + "://" +
-                            this.serverName +
-                            ":" + this.portNumber + "/",
-                    connectionProps);
-        } else {
-            System.out.println("Unknown database");
-            return null;
+    public void run(){
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can't get class. No driver found");
+            e.printStackTrace();
+            return;
         }
-        System.out.println("Connected to database");
-        return conn;
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(connectionString);
+            System.out.println("Connection has been established");
+        } catch (SQLException e) {
+            System.out.println("Can't get connection. Incorrect URL");
+            e.printStackTrace();
+            return;
+        }
+        createTable(connection);
+        try {
+            connection.close();
+            System.out.println("Connection has been closed");
+        } catch (SQLException e) {
+            System.out.println("Can't close connection");
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    private void createTable(Connection conn){
+        try {
+            String sql = "CREATE TABLE IF NOT EXISTS stuffs (\n"
+                    + "	id integer PRIMARY KEY,\n"
+                    + "	name text NOT NULL,\n"
+                    + "	price integer NOT NULL,\n"
+                    + "	count integer NOT NULL\n"
+                    + ");";
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SQLWorker app = new SQLWorker();
+        app.run();
     }
 }
