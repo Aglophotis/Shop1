3e831837fab7ca1be308f1a2c0a9562d14f73426
Luna <Atisphatis@gmail.com>
2018-09-21 23:54:21 +0300

    Small features

--- src/main/java/ru/mirea/data/SpringServer.java
+++ src/main/java/ru/mirea/data/SpringServer.java
@@ -2,7 +2,9 @@ package ru.mirea.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class SpringServer{
     public static void main(String[] args) {
